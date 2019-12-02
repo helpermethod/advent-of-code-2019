8f20fc00ace1a2368b2e2caf726f8dc05202e987
Oliver Weiler <oliver.weiler@meinestadt.de>
2019-12-02 09:16:27 +0100

    Remove local variables

--- day-1/part-1.kts
+++ day-1/part-1.kts
@@ -1,11 +1,10 @@
 #!/usr/bin/env kscript
+
 import java.io.File
 
-val sum = 
-    File("audit-smaller-sorted.txt")
-        .readLines()
-        .map(String::toDouble)      
-        .map { Math.floor(it / 3) - 2 }
-        .sum()
-
-println("$sum")
+File("audit-smaller-sorted.txt")
+    .readLines()
+    .map(String::toDouble)      
+    .map { Math.floor(it / 3) - 2 }
+    .sum()
+    .let { println(it)}
